27af9778e23e159e1c8fab03d5776b50ab32b3a2
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-11 11:44:32 +0300

    insert and custom print done

--- linked_list_exercise.py
+++ linked_list_exercise.py
@@ -1,3 +1,31 @@
+class Node:
+    def __init___(self,data=None,next=None):
+        self.data=data
+        self.next=next
+
+class LinkedList:
+    def __init___(self):
+        self.head=None
+
+    def insert_at_begining(self,data):
+        node=Node(data,self.head)
+        self.head=node
+    def print(self):
+        if self.head == None:
+            print('Linked list is empty')   
+            return
+        itr=self.head
+        llstr='' #linked list string
+        while itr:
+            llstr += str(itr) +'----->'
+            itr=itr.next
+        print(llstr)    
+
+
+
+
+
+
 def insert_after_value(self, data_after, data_to_insert):
     # Search for first occurance of data_after value in linked list
     # Now insert data_to_insert after data_after node
@@ -9,16 +37,19 @@ def remove_by_value(self, data):
 
 # make the following calls
 ll = LinkedList()
-ll.insert_values(["banana","mango","grapes","orange"])
-ll.print()
-ll.insert_after_value("mango","apple") # insert apple after mango
-ll.print()
-ll.remove_by_value("orange") # remove orange from linked list
-ll.print()
-ll.remove_by_value("figs")
-ll.print()
-ll.remove_by_value("banana")
-ll.remove_by_value("mango")
-ll.remove_by_value("apple")
-ll.remove_by_value("grapes")
+# ll.insert_values(["banana","mango","grapes","orange"])
+# ll.print()
+# ll.insert_after_value("mango","apple") # insert apple after mango
+# ll.print()
+# ll.remove_by_value("orange") # remove orange from linked list
+# ll.print()
+# ll.remove_by_value("figs")
+# ll.print()
+# ll.remove_by_value("banana")
+# ll.remove_by_value("mango")
+# ll.remove_by_value("apple")
+# ll.remove_by_value("grapes")
+# ll.print()
+ll.insert_at_begining(78)
+ll.insert_at_begining(38)
 ll.print()
