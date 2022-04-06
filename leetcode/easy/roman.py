f9d5781b9f444ab72f17abbe9170513bfdb230d2
JohnM-Kahura <Johnmungai392@gmail.com>
2022-04-07 01:22:32 +0300

    leetcode q no 1

---
+++ leetcode/easy/roman.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def romanToInt(self, s):
+        
+        symbol_value={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000,
+            
+            
+        }
+        for i in s.split():
+            pass
+            
+    
+        
