5069097b0ccd370c1844ea89e6e3325ee8f051c9
JohnM-Kahura <Johnmungai392@gmail.com>
2022-05-09 13:52:51 +0300

    .

--- fib_seq.py
+++ fib_seq.py
@@ -3,4 +3,19 @@ def fib(n):
         return 1
     return fib(n-1)+fib(n-2)
 print(fib(10))
-    
+# print(fib(50))
+# fib with memoization
+def fib_2(n,memo):
+    if memo[n] is not  None:
+        return memo[n]
+    if n==1 or n==2:
+        result=1
+    else:
+        result=fib_2(n-1,memo)+fib_2(n-2,memo)
+
+    memo[n]=result
+    return result   
+def fib_memo(n):
+    memo=[None] *(n+1)
+    return fib_2(n,memo)
+print(fib_memo(50))
