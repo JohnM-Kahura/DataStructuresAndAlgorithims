b3e4b52b59bcd9df335fd0904778aa179463285c
JohnM-Kahura <Johnmungai392@gmail.com>
2022-07-06 14:38:11 +0300

    nonesense

---
+++ hospital.py
@@ -0,0 +1,11 @@
+
+# number of branches for the deliverly of medicine
+# number of types of existing drugs
+# number of patients to attend to
+
+
+def hospital(n,k,m):
+
+    if(n or k <1):
+        print('You must read carefully!!')
+    
