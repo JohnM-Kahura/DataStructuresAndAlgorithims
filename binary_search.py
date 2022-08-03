4bf49b054b757bc3e08040bf7b73ff89209976a3
JohnM-Kahura <Johnmungai392@gmail.com>
2022-08-03 15:25:47 +0300

    binary search implementation

---
+++ binary_search.py
@@ -0,0 +1,12 @@
+arr=[1,2,3,4,5,6,7,8,9]
+start=0
+end=len(arr)-1
+target=9
+
+def binarySearch(arr,start,end,target):
+    midIndex=start+end /2
+    if target==arr[midIndex]: return True
+    if arr[midIndex]>target: return binarySearch(arr,start,midIndex-1,target)
+    else :return binarySearch(arr,midIndex+1,end,target)
+
+print(binarySearch(arr,start,end,target))
